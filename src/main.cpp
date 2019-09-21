90f3c5619541e057a4a2afc1b3b881a48bcb2c61
piotrkowandy <piotr.kowandy@o2.pl>
2019-09-21 22:50:11 +0200

    fight with curl

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include "CourseDashboard.hpp"
 #include "User.hpp"
-
+#include "Server.hpp"
 
 int main()
 {
     CourseDashboard cdb;
     std::cout << "Course Dashboard" << std::endl;
 
-    cdb.saveToFile("userDatabase.txt");
-    cdb.loadFromFile("userDatabase.txt");
+    Server server1;   
+
+    if (server1.checkIfFileExist())
+    {
+        std::cout << "OK\n";
+        server1.downloadUsers();
+        server1.checkConnection();
+    }
+    else
+        std::cout << "NOT OK\n";
+
+    //cdb.saveToFile("userDatabase.txt");
+    //cdb.loadFromFile("userDatabase.txt");
 
     return 0;
 }
